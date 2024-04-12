23720def68e400d02c7e5990b16d877f261dc63a
shreyashhiwase <122458605+shreyashhiwase@users.noreply.github.com>
2024-04-12 16:47:43 +0530

    Add files via upload

---
+++ app-files/data.js
@@ -0,0 +1,38 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-4cf47ce1178be67a7cb890e09233b9e0-01",
+      "name": "4cf47ce1178be67a7cb890e09233b9e0-01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.22383358683914878,
+        "pitch": 0.07736015999128476,
+        "fov": 1.5537519122748535
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
